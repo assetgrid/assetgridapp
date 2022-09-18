ee9d3015a7b12ee455d3605b4b8375e0b2a2cb41
Alex <alexander@svorre.dk>
2022-09-18 17:44:36 +0200

    Fix create account page

--- homebudget-frontend/Roadmap.md
+++ homebudget-frontend/Roadmap.md
@@ -50,4 +50,4 @@
 - [ ] Finish dashboard
 - [ ] Fix search
 - [ ] Fix account and transaction creation pages
-- [ ] Add property to transaction called type that can be deposit, withdrawal or transfer. Depends on whether source, destination or both are included in net worth. Transactions should be colored according to type on transaction page and it should be possible to filter transactions based on type
+- ~~[ ] Add property to transaction called type that can be deposit, withdrawal or transfer. Depends on whether source, destination or both are included in net worth. Transactions should be colored according to type on transaction page and it should be possible to filter transactions based on type~~
--- homebudget-frontend/src/components/pages/account/PageCreateAccount.tsx
+++ homebudget-frontend/src/components/pages/account/PageCreateAccount.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
+import { useNavigate } from "react-router";
+import { Api } from "../../../lib/ApiClient";
+import { routes } from "../../../lib/routes";
+import { Account, CreateAccount } from "../../../models/account";
+import AccountLink from "../../account/AccountLink";
+import { preferencesContext } from "../../App";
 import { Card } from "../../common/Card";
 import InputButton from "../../input/InputButton";
-import CreateTransaction from "../../transaction/CreateTransaction";
+import InputCheckbox from "../../input/InputCheckbox";
+import InputText from "../../input/InputText";
+
+const defaultAccount = {
+    name: "",
+    accountNumber: "",
+    description: "",
+    favorite: true,
+    includeInNetWorth: true,
+};
 
 export default function () {
+    const [value, setValue] = React.useState<CreateAccount>(defaultAccount);
+    const [isCreating, setIsCreating] = React.useState(false);
+    const { preferences, updatePreferences } = React.useContext(preferencesContext);
+    const navigate = useNavigate();
+    const [createdAccount, setCreatedAccount] = React.useState<Account | null>(null);
+
     return <>
         <section className="hero has-background-info" style={{ flexDirection: "row", alignItems: "center" }}>
             <div className="hero-body">
@@ -13,14 +34,63 @@ export default function () {
             </div>
         </section>
         <div className="p-3">
-            <Card title="Create transaction">
-                <CreateTransaction />
+            <Card title="Create account">
+                {createdAccount && <article className="message is-link">
+                        <div className="message-body">
+                        Account has been created: <AccountLink account={createdAccount} />
+                        </div>
+                </article>}
+
+                <InputText label="Name"
+                    value={value.name}
+                    onChange={e => setValue({ ...value, name: e.target.value })}
+                    disabled={isCreating} />
+                <InputText label="Description"
+                    value={value.description}
+                    onChange={e => setValue({ ...value, description: e.target.value })}
+                    disabled={isCreating} />
+                <InputText label="Account number"
+                    value={value.accountNumber}
+                    onChange={e => setValue({ ...value, accountNumber: e.target.value })}
+                    disabled={isCreating} />
+                <InputCheckbox label="Favorite"
+                    value={value.favorite}
+                    onChange={e => setValue({ ...value, favorite: e.target.checked })}
+                    disabled={isCreating} />  
+                <InputCheckbox label="Include in net worth"
+                    value={value.includeInNetWorth}
+                    onChange={e => setValue({ ...value, includeInNetWorth: e.target.checked })}
+                    disabled={isCreating} />  
 
                 <div className="buttons">
-                    <InputButton onClick={() => 0}>Create and Continue</InputButton>
-                    <InputButton onClick={() => 0}>Create and View Transaction</InputButton>
+                    <InputButton onClick={() => create(true)} disabled={isCreating}>Create and stay</InputButton>
+                    <InputButton onClick={() => create(false)} disabled={isCreating}>Create and view account</InputButton>
                 </div>
             </Card>
         </div>
     </>;
+
+    async function create(stay: boolean) {
+        setIsCreating(true);
+        setCreatedAccount(null);
+        const result = await Api.Account.create(value);
+        setIsCreating(false);
+        if (result.favorite) {
+            if (preferences === "fetching") {
+                updatePreferences(null);
+            } else {
+                updatePreferences({
+                    ...preferences,
+                    favoriteAccounts: [...preferences.favoriteAccounts, result]
+                });
+            }
+        }
+        
+        if (stay) {
+            setValue(defaultAccount);
+            setCreatedAccount(result);
+        } else {
+            navigate(routes.account(result.id.toString()));
+        }
+    }
 }
